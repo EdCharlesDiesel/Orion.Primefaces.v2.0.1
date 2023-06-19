bad0657e33d4d5b9c0d1114f7dbf297e45d205ec
Khotso <Mokhetkc@hotmail.com>
2023-06-19 15:30:24 +0200

    Bug Fixes.

--- src/app/orion/pages/user-login/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    userId: number | undefined;
-    firstName: string| undefined;
-    lastName: string| undefined;
-    emailAddress: string| undefined;
-    username: string| undefined;
-    userTypeId: number| undefined;
-    isLoggedIn: boolean| undefined;
-}
