e1f8f17b62c9560a1aed849bdd8c674db3d6e68f
Khotso <Mokhetkc@hotmail.com>
2023-06-20 10:58:41 +0200

    Fixing the UI.

--- src/app/orion/pages/apps/apps-routing.module.ts
+++ src/app/orion/pages/apps/apps-routing.module.ts
@@ -3,9 +3,12 @@ import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [RouterModule.forChild([
-     {path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
-    {path: 'calendar', loadChildren: () => import('./calendar/calendar-routing.module').then(m => m.CalendarRoutingModule)},
-    {path: 'reports', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
+    {path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
+    {
+      path: 'calendar',
+      loadChildren: () => import('./calendar/calendar-routing.module').then(m => m.CalendarRoutingModule)
+    },
+    {path: 'reports', loadChildren: () => import('./reports/reports-routing.module').then(m => m.ReportsRoutingModule)},
     {path: 'files', loadChildren: () => import('./files/files.module').then(m => m.FilesModule)},
     {path: 'mail', loadChildren: () => import('./mail/mail-routing.module').then(m => m.MailRoutingModule)},
     {path: 'task-list', loadChildren: () => import('./task-list/task-list.module').then(m => m.TaskListModule)},
